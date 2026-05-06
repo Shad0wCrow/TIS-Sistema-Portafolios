43332bdc4cdf5e7264ddcefd31d75b3a2bae7815
M1guelang <almanzaa9928@gmail.com>
2026-05-05 22:48:48 -0400

    Fix Navegar portafolio

--- front-portafolio/src/components/ComponentsHome/Sidebar.tsx
+++ front-portafolio/src/components/ComponentsHome/Sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar: React.FC = () => {
 
       case 'portafolio':
         if (hasPortafolio) {
-          navigate('/portafolio/editar');
+          navigate('/portafolio');
         } else {
           setShowModal(true);
         }
